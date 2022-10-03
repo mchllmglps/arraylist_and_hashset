d5e3d0cd7af9e571fdac9c54643b213080d0373f
Mich <mchllmglps@gmail.com>
2022-10-03 15:45:58 +0800

    own exception

---
+++ 10.03.22/BooksException.java
@@ -0,0 +1,8 @@
+public class BooksException extends Exception{
+    // public String negative(){
+    //     return "Your input in negative. Please input positive index.";
+    // }
+    public BooksException(String message){
+        super(message);
+    }
+}
